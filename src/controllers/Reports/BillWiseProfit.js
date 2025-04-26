bce409e556e98c63ad55d1d628be087ff6dee893
Krishpatel3085 <f388krish@gmail.com>
2025-04-26 14:09:24 +0530

    Add some change

--- src/controllers/Reports/BillWiseProfit.js
+++ src/controllers/Reports/BillWiseProfit.js
@@ -2,5 +2,6 @@ import MESSAGES from "../../config/messages.js";
 import getCompanyModels from "../../utils/getCompanyModels.js";
 
 const BillWise = async (req, res) =>{
-
+// Comming soon ................
+// Under Survaliance
 }
